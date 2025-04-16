2668bb8075827e717189e1a6e93e8a2ca06db0c2
Albert Huang <17867974805@163.com>
2025-04-16 17:09:08 +0800

    前端配置+框架

--- web/src/components/widgets/sidebar/index.tsx
+++ web/src/components/widgets/sidebar/index.tsx
@@ -27,14 +27,7 @@ const navItems =[
         path : "/plan"
     },
 ]
-const data = {
-    user: {
-        name: "username",
-        email: "m@example.com",
-        avatar: "https://avatars.githubusercontent.com/u/149759599?v=4",
-    },
 
-}
 const infoItem = [
     {
         title : "settings",
