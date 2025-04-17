de640717048a5ca976831981161420330ccca7cf
Lei Lecen <145575708+llc141525@users.noreply.github.com>
2025-04-17 20:20:10 +0800

    Llc2 (#16)
    * 登录和注册完成了
    
    * 清理了一下代码
    
    * 将要重构字段
    
    * 暂存, 修改一下user代码, 重构一下异常处理.
    
    * 登陆注册的异常处理完成了
    
    * 任务的创建完成了, 接下来做todolist
    
    * 基本的异常框架搭好了
    
    * task 完成了
    
    * 基本完成. 但是获取所有任务的api 需要改一下, 返回的tasks 有问题
    
    * 编写了一些测试类
    
    * 时间戳修改好了, 密码加密了, 增量更新完成了,还差一个过滤器
    
    * 拦截器配置完成

--- src/main/java/org/example/todo_list/controller/TaskController.java
+++ src/main/java/org/example/todo_list/controller/TaskController.java
@@ -3,7 +3,6 @@ package org.example.todo_list.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.example.todo_list.dto.request.CreateTaskRequest;
@@ -11,21 +10,19 @@ import org.example.todo_list.dto.request.UpdateTaskRequest;
 import org.example.todo_list.dto.response.GetTaskResponse;
 import org.example.todo_list.service.TaskService;
 import org.example.todo_list.utils.ApiResponse;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "任务相关Api", description = "用于管理任务")
 @RestController
 @RequestMapping("/task")
 @RequiredArgsConstructor
-@Validated
 public class TaskController {
 
     private final TaskService taskService;
 
     @Operation(summary = "新建一个任务",
             description = "传入任务名, 类别.这两个是必须的. 还有非必须的 任务备注, 任务状态(是否完成, true 或者 false), 截至日期(时间戳)")
-    @PostMapping(value = {"/", ""})
+    @PostMapping({"/", ""})
     public ApiResponse<String> createTask(@Valid @RequestBody CreateTaskRequest createTaskRequest) {
         taskService.createTask(createTaskRequest);
         return ApiResponse.success("创建任务成功");
@@ -34,44 +31,22 @@ public class TaskController {
     @Operation(summary = "获取一个任务",
             description = "传入任务id, 返回 task的详细信息")
     @GetMapping("/{id}")
-    public ApiResponse<GetTaskResponse> getTask(@Min(value = 1, message = "id至少为1")
-                                                @PathVariable Long id) {
+    public ApiResponse<GetTaskResponse> getTask(@PathVariable Long id) {
         return ApiResponse.success(taskService.getTask(id));
     }
 
     @Operation(summary = "更新任务对应的参数")
-    @PatchMapping(value = {"/{id}"})
-    public ApiResponse<?> updateTask(@NotNull @PathVariable("id") Long id,
-                                     @RequestBody UpdateTaskRequest updateTaskRequest) {
+    @PatchMapping({"/{id}"})
+    public ApiResponse<?> updateTask(@PathVariable Long id,
+                                     @Valid @RequestBody UpdateTaskRequest updateTaskRequest) {
         taskService.updateTask(id, updateTaskRequest);
         return ApiResponse.success(null);
     }
 
-//    @Operation(summary = "把一个任务标记为已完成")
-//    @PatchMapping("/finish")
-//    public ApiResponse<String> finishTask(@NotNull @RequestParam Long id) {
-//        taskService.changeStatus(id, true);
-//        return ApiResponse.success("任务完成");
-//    }
-
-//    @Operation(summary = "把已经完成的任务标记为未完成")
-//    @PatchMapping("/not_finish")
-//    public ApiResponse<String> notFinishTask(@NotNull @RequestParam Long id) {
-//        taskService.changeStatus(id, false);
-//        return ApiResponse.success("成功修改任务状态");
-//    }
-
     @Operation(summary = "删除一个任务")
-    @DeleteMapping(value = {"/{id}"})
+    @DeleteMapping({"/{id}"})
     public ApiResponse<String> deleteTask(@NotNull @PathVariable("id") Long id) {
         taskService.deleteTask(id);
         return ApiResponse.success("删除成功");
     }
-
-//    @Operation(summary = "修改截止日期")
-//    @PatchMapping("/change_deadline")
-//    public ApiResponse<String> changeDeadline(@NotNull @RequestParam Long id, @NotNull Long deadline) {
-//        taskService.changeDeadline(id, deadline);
-//        return ApiResponse.success("成功修改截至日期");
-//    }
 }
--- src/main/java/org/example/todo_list/exception/errors/UserError.java
+++ src/main/java/org/example/todo_list/exception/errors/UserError.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 public enum UserError {
     USER_NOT_FOUND(1001, "用户不存在"),
     INVALID_PASSWD_USER(1002, "用户名或者密码错误"), // 登陆的时候用户名或者密码错误
-    DUPLICATE_USERNAME(1003, "用户名已经存在");     // 注册的时候不能有重复的用户名
-
+    DUPLICATE_USERNAME(1003, "用户名已经存在"),     // 注册的时候不能有重复的用户名
+    NO_COOKIE(1004, "没有登录");
 
     private final Integer code;
     private final String message;
