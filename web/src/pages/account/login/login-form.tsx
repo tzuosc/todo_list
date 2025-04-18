f6b7e2211abfaf19bea526dc4597d504dcbdd41c
Albert Huang <149759599+Albertknight2023@users.noreply.github.com>
2025-04-18 12:09:04 +0800

    Hyy (#17)
    * i
    
    * aa
    
    aa
    
    * 前端配置
    
    * 前端配置+框架
    
    * 2024年8月16日
    
    * 前端配置+框架
    
    * 前端配置+框架
    
    * 前端配置+框架
    
    * 前端配置+框架
    
    * 111
    
    111
    
    * 前端配置+框架
    
    * aaa
    
    * login+register
    
    ---------
    
    Co-authored-by: Lei Lecen <145575708+llc141525@users.noreply.github.com>

--- web/src/pages/account/login/login-form.tsx
+++ web/src/pages/account/login/login-form.tsx
@@ -1,39 +1,108 @@
 import { cn } from "@/utils";
 
-
+import {toast} from "sonner";
 import { Input } from "@/components/ui/input.tsx";
-import { Label } from "@/components/ui/label.tsx";
+
 import { Button } from "@/components/ui/button.tsx";
+import { useAuthStore } from "@/storages/auth.ts";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { login } from "@/api/user";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form.tsx";
 
 function LoginForm(){
+    const authStore =useAuthStore()
+    const navigate =useNavigate()
+    const [loading,setLoading] = useState<boolean>(false)
+    const formSchema =z.object({
+        username:z.string({
+            message:"请输入用户名",
+        }),
+        password:z.string({
+            message:"请输入密码"
+        })
+    })
+    const form = useForm<z.infer<typeof formSchema> >({
+        resolver:zodResolver(formSchema)
+    })
+    function  onSubmit(values:z.infer<typeof formSchema>){
+        setLoading(true)
+        login({
+            ...values
+        })
+            .then((res)=>{
+            if(res.code===200){
+                authStore.setUser(res.data)
+            toast.success("success to login",{
+                id:"login-success",
+                description:`welcome back,${res.data?.username}`
+                })
+                navigate("/")
+            }
+            if (res.code===400){
+                toast.error(
+                    "fall to login",{
+                        id:"login-erro",
+                        description:res.msg
+                    }
+                )
+            }
+        })
+            .finally(()=>{
+                setLoading(false)
+            })
+    }
     return(
-        <form className="px-6 md:px-8">
+        <Form {...form}>
+        <form className="px-6 md:px-8" onSubmit={form.handleSubmit(onSubmit)}>
             <div className="flex flex-col gap-6">
                 <div className={cn("flex flex-col items-center text-center")}>
                     <h1 className="text-2xl font-bold">Welcome back</h1>
                     <p className="text-balance text-muted-foreground">Login to your Acme Inc account</p>
                 </div>
-                <div className="grid gap-2">
-                    <Label htmlFor="username">username</Label>
-                    <Input id="username"  placeholder="username" />
-                </div>
-                <div className="grid gap-2">
-                    <div className="flex items-center">
-                        <Label htmlFor="password">Password</Label>
-                        <a href="#" className="ml-auto text-sm underline-offset-2 hover:underline">
-                            Forgot your password?
-                        </a>
-                    </div>
-                    <Input id="password" type="password" required />
-                </div>
-                <Button type="submit" className="w-full">
+                <FormField
+                control={form.control}
+                name={'username'}
+                render={({field }) => (
+                    <FormItem>
+                        <FormLabel>用户名</FormLabel>
+                        <FormControl>
+                            <Input
+                                placeholder={"Login Name"}
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+                />
+                <FormField
+                    control={form.control}
+                    name={"password"}
+                    render={({field})=>(
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input
+                                placeholder={"password"}
+                                type={"password"}
+                                {...field}
+                                ></Input>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                ></FormField>
+                <Button type="submit" className="w-full" loading={loading}>
                     Login
                 </Button>
 
 
             </div>
         </form>
-
+        </Form>
     )
 }
 export {LoginForm}
