726ddca4c0efdd304d1ad222904030e2a1edf710
Lei Lecen <145575708+llc141525@users.noreply.github.com>
2025-04-19 13:52:14 +0800

    Revert "bbb"
    This reverts commit d50625981e84bb41d23473f3b5c0ac6c5e698dd5.

---
+++ src/main/java/org/example/todo_list/dto/request/LoginRequest.java
@@ -0,0 +1,21 @@
+package org.example.todo_list.dto.request;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+
+/*登录或者注册的 DTO*/
+
+@Schema(name = "登录请求")
+@Builder
+public record LoginRequest(
+        @Schema(name = "username", example = "test")
+        @NotBlank(message = "用户名不能为空") String username,
+
+        @Schema(name = "password", example = "123456")
+        @NotNull(message = "缺少密码字段") @NotBlank(message = "密码不能为空") String password
+) {
+}
