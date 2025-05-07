0eff7a1731ac0c70deb476295a7207e5c296914a
Lei Lecen <145575708+llc141525@users.noreply.github.com>
2025-05-07 21:27:49 +0800

    Llc (#36)
    * test
    
    * 版本管理
    
    * finish
    
    * 暂存
    
    * 暂存
    
    * 教程更新
    
    * finish
    
    * a

--- src/main/java/org/example/todo_list/repository/TodoListRepository.java
+++ src/main/java/org/example/todo_list/repository/TodoListRepository.java
@@ -14,22 +14,5 @@ import java.util.Optional;
 
 @Repository
 public interface TodoListRepository extends JpaRepository<TodoList, Long> {
-    @Transactional
-    @Modifying
-    @Query("update TodoList t set t.category = ?1 where t.id = ?2")
-    int updateCategoryById(String category, Long id);
-
-    @jakarta.transaction.Transactional
-    @Query("select t from TodoList t left join fetch t.tasks where t.user.id= ?1")
-    List<TodoList> findTodoListByUserId(Long userId);
-
-    @Transactional
-    @Query("select count(t)>0 from TodoList t  where t.user.id = ?2 and t.category=?1")
-    boolean existsByCategory(String category, Long UserId);
-
-    @Transactional
-    @Query("select t from TodoList t  where t.user.id = ?2 and t.category=?1")
-    Optional<TodoList> findByCategory(String category, Long UserId);
-
 
 }
